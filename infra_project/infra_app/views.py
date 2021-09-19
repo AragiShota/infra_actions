832b6276d4ba56b16c74db86e0fc8e9a75b6cf61
Aragi Shota <stas131000@yandex.ru>
2021-09-19 14:12:21 +0300

    YEAH!

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!!!')
+    return HttpResponse('Почему мы всё ещё здесь?')
 
 
 def second_page(request):
